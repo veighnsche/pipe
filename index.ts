2c6c23c6777ab84054e7d7cce0ebd4c687720ffe
Vince Paul Liem <vincepaul.liem@gmail.com>
2020-12-05 14:40:25 +0100

    added actionpipe

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export type NextFunction = (next: Function, args?: any) => void
+export type Action = { type: any, payload?: any }
+export type State = Record<any, any>
 
 let logging = false
 
@@ -25,3 +27,12 @@ export function asyncPipe(...functions: NextFunction[]) {
   (functions.reverse().reduce((chain, f) => (args: any) => f(chain, log(f, args))) as () => NextFunction)()
 }
 
+export function actionPipe(reducer: (prevState: State, action: Action) => State) {
+  return function (...actions: ((payload: any) => Action)[]) {
+    return function (state: State, payload: any) {
+      return actions.reduce((nextStore, action) => {
+        return reducer(nextStore, action(payload))
+      }, state)
+    }
+  }
+}
